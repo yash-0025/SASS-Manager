c857b820d0084fdd3fd431c74b2f6827aa04b9b6
yash-0025 <yashpatel200403@gmail.com>
2025-05-19 01:55:32 +0530

    # BACKEND :: UPDATE USER BY ID CONTROLLER CREATED

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -31,3 +31,36 @@ exports.getUserById = async(req,res) => {
         })
     }
 }
+
+exports.updateUserById = async(req,res) => {
+    try {
+        const id = req.params.id;
+
+        const {name, email, password, isAdmin, isSuperAdmin} = req.body;
+        const options = {new: true}
+        const hashedPassword = await bcrypt.hash(password,10);
+        if(password.length<6) return res.status(400).json({
+            message: "Password should have minimum length of 6 characters"
+        })
+        const result = await User.findByIdAndUpdate(
+            id, {
+                name:name,
+                email:email,
+                password: hashedPassword,
+                isAdmin: isAdmin,
+                isSuperAdmin: isSuperAdmin
+            }
+        )
+        console.log(result);
+        if(!result) {
+            res.status(500).json({
+                message: "Update Failed"
+            })
+        }
+        res.status(result)
+    } catch(error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
