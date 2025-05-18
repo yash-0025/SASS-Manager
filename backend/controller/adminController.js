c7d2f89f68a1717a7c89dbfd04b94e95a54e2582
yash-0025 <yashpatel200403@gmail.com>
2025-05-19 02:05:05 +0530

    # BACKEND :: ADMIN ALL SERVICES CONTROLLER CREATED

--- backend/controller/adminController.js
+++ backend/controller/adminController.js
@@ -1,12 +1,24 @@
 const express = require('expres')
 const {User, Servie, Cart} = require('../models')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const Service = require('../models/Service');
 
 
 exports.allUsers = async(req,res) => {
     try {
         const data = await User.find();
         res.json(data)
+    } catch(error) {
+        res.status(500).json({
+            message:error.message
+        })
+    }
+}
+
+exports.allServies = async(req,res) => {
+    try{
+        const data = await Service.find()
+        res.json(data);
     } catch(error) {
         res.status(500).json({
             message:error.message
