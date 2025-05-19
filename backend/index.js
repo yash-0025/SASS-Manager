43f23d549309427ccb98aa08414145685b46cfcb
yash-0025 <yashpatel200403@gmail.com>
2025-05-20 02:13:08 +0530

    # BACKEND :: INDEX FILE UPDATED

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,20 @@ const express = require('express')
 const dotenv= require('dotenv')
 dotenv.config({})
 const mongoose = require('mongoose')
+const cors = require('cors')
 
+mongoose.set("strictQuery", false)
+
+// ROUTES IMPORTS
+const authRoutes = require("./routes/authRoutes.js")
+const adminRoutes = require("./routes/adminRoutes.js")
+const cartRoutes = require("./routes/cartRoute.js")
+const paymentRoutes = require("./routes/paymentRoutes.js")
+const serviceRoutes = require("./routes/serviceRoutes.js")
+const userRoutes = require("./routes/userRoutes.js")
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 
@@ -25,6 +36,12 @@ mongoose.connection.once('connected', () => {
 
 
 // ROUTES
+app.use('/',authRoutes)
+app.use('/api',userRoutes)
+app.use('/api',serviceRoutes)
+app.use('/api',adminRoutes)
+app.use('/api',cartRoutes)
+app.use('/api/payment',paymentRoutes)
 
 
 // SERVER CONNECTION
