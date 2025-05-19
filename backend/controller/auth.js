08b828e0f3930e8b030fedee5141586ffb757997
yash-0025 <yashpatel200403@gmail.com>
2025-05-20 01:50:00 +0530

    # BACKEND :: AUTH ROUTES CREATED

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -1,5 +1,4 @@
 require('dotenv').config({path: '../'});
-const express = require('express')
 const {User} = require("../models/User")
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,2 +1,10 @@
+const { signup, login } = require("../controller/auth")
+
 const express = requie('express')
-const router 
+const router = express.Router()
+
+
+router.post('/signup', signup)
+router.post('/login',login)
+
+module.exports = router
