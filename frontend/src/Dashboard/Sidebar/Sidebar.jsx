65226d46f7831a45c7dbcedc3161a5de180b0aff
yash-0025 <yashpatel200403@gmail.com>
2025-05-21 23:17:18 +0530

    # FRONTEND :: SIDEBAR COMPONENT CREATED

---
+++ frontend/src/Dashboard/Sidebar/Sidebar.jsx
@@ -0,0 +1,45 @@
+import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import StoreIcon from "@mui/icons-material/Store";
+import InsertChartIcon from "@mui/icons-material/InsertChart";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import { Link } from "react-router-dom";
+
+import { Grid } from "@mui/material";
+
+const Sidebar = () => {
+    return (
+
+    <Grid item xs={2}  position="sticky">
+        <div className="sidebar" >
+            <div className="center">
+                <ul >
+                    <Link to="/users" style={{ textDecoration: "none" }}>
+                        <li>
+                            <PersonOutlineIcon className="icon" />
+                            <span>Users</span>
+                        </li>
+                    </Link>
+                    <Link to="/products" style={{ textDecoration: "none" }}>
+                        <li>
+                            <StoreIcon className="icon" />
+                            <span>Services</span>
+                        </li>
+                    </Link>
+                </ul>
+            </div>
+
+        </div>
+    </Grid>
+    );
+};
+
+export default Sidebar;
